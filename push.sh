910e4f5293a351447383b417cd761dd8c5927b5f
ryanemax <ryanemax@icloud.com>
2016-02-02 11:10:58 -0500

    update

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ mv ramlnova bin/linux64/ramlnova
 cp -rf template bin/linux64/template
 # rsync bin/ramlnova root@api.anasit.com:/opt/anasit/ramlnova
 git add .
-git commit -m "up"
+git commit -m "update"
 git push -u origin master
 
